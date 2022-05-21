0c1ab8bf1e5a0b78390ab42a725e3eca2498e012
EGYuda23 <egyyudanugraha@gmail.com>
2022-05-21 12:15:19 +0700

    Add cors

--- src/server.js
+++ src/server.js
@@ -9,7 +9,12 @@ const router = require('./Routes/router');
 const init = async () => {
   const server = Hapi.server({
     port: process.env.PORT,
-    host: process.env.HOST,
+    host: process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
   });
 
   await server.register(JWT);
