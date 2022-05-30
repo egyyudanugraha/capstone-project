a478211e265daa807b6ee5a302e7dc9731a6358e
Sely Saputri <selyputri003@gmail.com>
2022-05-30 16:24:17 +0700

    add article item layout

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
