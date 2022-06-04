bb86cf10a3c3a5aeea674046275ce2db5b4b84f0
I Nyoman Arijaya Putra <63515581+ArijayaPutra@users.noreply.github.com>
2022-06-05 01:55:13 +0700

    Update routes.js
    - Nambahin routes pomodoro

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,5 +1,6 @@
 import Home from '../views/pages/home';
 import Matrix from '../views/pages/matrix';
+import Pomodoro from '../views/pages/pomodoro';
 import Task from '../views/pages/task';
 import Login from '../views/pages/login';
 import Register from '../views/pages/register';
@@ -9,7 +10,7 @@ const routes = {
   '/': Home,
   '/tasks': Task,
   '/matrix': Matrix,
-  '/pomodoro': 'pomodoro',
+  '/pomodoro': Pomodoro,
   '/profile': Profile,
   '/login': Login,
   '/register': Register,
