fc55edb15e3305221390c26ad2364e0fd150f138
EGYuda23 <egyyudanugraha@gmail.com>
2022-06-04 17:05:50 +0700

    add cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 const matrixRouter = require('./routers/matrix');
@@ -7,6 +8,7 @@ require('./db/mongoose');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use([userRouter, taskRouter, matrixRouter, historyRouter]);
 
