d7a93e95119cbe8222fa513173ef8d865e340c82
EGYuda23 <egyyudanugraha@gmail.com>
2022-06-08 06:58:25 +0700

    edit config

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,6 +1,6 @@
-const url = process.env.URL_API;
-const urlNews = process.env.URL_NEWS;
-const apiKey = process.env.API_KEY;
+const url = `${process.env.URL_API}`;
+const urlNews = `${process.env.URL_NEWS}`;
+const apiKey = `${process.env.API_KEY}`;
 
 const CONFIG = {
   BASE_URL: url,
