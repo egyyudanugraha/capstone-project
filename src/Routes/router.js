3f286ea1cc8519007522f51d65eb6916f1c73350
adityaSahrin <99312087+adityaSahrin@users.noreply.github.com>
2022-06-12 17:17:58 +0700

    Delete src directory

--- src/Routes/router.js
+++
@@ -1,9 +0,0 @@
-const userRoute = require('./user');
-const taskRoute = require('./task');
-const authRoute = require('./auth');
-const historyRoute = require('./history');
-const articleRoute = require('./article');
-
-const router = [].concat(authRoute, userRoute, taskRoute, historyRoute, articleRoute);
-
-module.exports = router;
