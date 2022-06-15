0ba9de9739a75f9bb610f8d442e03e459a6be56d
EGYuda23 <egyyudanugraha@gmail.com>
2022-06-15 14:36:55 +0700

    change state history

--- src/models/task.js
+++ src/models/task.js
@@ -1,46 +1,49 @@
 const mongoose = require('mongoose');
 const History = require('./history');
 
-const taskSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+      default: '',
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    urgency: {
+      type: Number,
+      default: 0,
+      max: 5,
+    },
+    important: {
+      type: Boolean,
+      default: false,
+    },
+    deadline: {
+      type: Number,
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
   },
-  description: {
-    type: String,
-    trim: true,
-    default: '',
+  {
+    timestamps: true,
   },
-  completed: {
-    type: Boolean,
-    default: false,
-  },
-  urgency: {
-    type: Number,
-    default: 0,
-    max: 5,
-  },
-  important: {
-    type: Boolean,
-    default: false,
-  },
-  deadline: {
-    type: Number,
-    required: true,
-  },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'User',
-  },
-}, {
-  timestamps: true,
-});
+);
 
 taskSchema.pre('save', async function (next) {
   const task = this;
-  if (task.isModified('completed') && task.completed === false) {
+  if (task.isModified('completed') && !task.completed) {
     await History.findOneAndDelete({ task: task._id });
   }
   next();
