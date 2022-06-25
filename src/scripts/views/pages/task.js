6f9d91ac6926b29ca91357a56e16144f4c26b753
EGYuda23 <egyyudanugraha@gmail.com>
2022-06-25 15:14:36 +0700

    finish commit

--- src/scripts/views/pages/task.js
+++ src/scripts/views/pages/task.js
@@ -14,7 +14,7 @@ const Task = {
               <textarea
                 id="description"
                 class="resize-none bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                rows="7"
+                rows="9"
                 placeholder="Description (optional)"
               ></textarea>
             </div>
